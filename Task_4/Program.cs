a82cd096b4af217af079af15f72eb5adacc454be
MaximBabenkov <lostbass@mail.ru>
2022-05-13 22:19:25 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()??"0");
+int start = 1;
+while(start<=a)
+{
+    if(start%2==0)
+    {
+        Console.Write($" {start}");
+        start++;
+    }
+    else
+    {
+        start++;
+    }
+}
