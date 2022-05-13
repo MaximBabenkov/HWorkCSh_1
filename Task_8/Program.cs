beca9563cdf718b408c4c28a89da08091206d5ad
MaximBabenkov <lostbass@mail.ru>
2022-05-13 23:04:22 +0300

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить - кратно ли число заданному, если нет - вывести остаток
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Ввдите второе число");
+int b = int.Parse(Console.ReadLine()??"0");
+if(a%b==0)
+{
+    Console.WriteLine($"{a} кратно {b}");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления: {a%b}");
+}
