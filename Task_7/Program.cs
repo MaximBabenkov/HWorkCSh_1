046281a77a696633230ec85e9e4c0ded0bd57631
MaximBabenkov <lostbass@mail.ru>
2022-05-13 22:55:54 +0300

    Task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,4 @@
+﻿// Удалить вторую цифру трехзначного числа
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine($"Вторая цифра исчезла: {(a/100)*10+(a%10)}");
