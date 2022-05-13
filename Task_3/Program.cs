2b381f44644188985c29c22b7ce96060a11c2bd7
MaximBabenkov <lostbass@mail.ru>
2022-05-13 22:11:28 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить - является ли число четным
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()??"0");
+if(a%2==0)
+{
+    Console.WriteLine("Это число - четное");
+}
+else
+{
+    Console.WriteLine("Это число - нечетное");
+}
