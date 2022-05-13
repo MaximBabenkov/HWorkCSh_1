ff474697d82e2089f1f8ba542fc7cad96a3b96f8
MaximBabenkov <lostbass@mail.ru>
2022-05-13 22:26:17 +0300

    Task_5

---
+++ Task_5/Program.cs
@@ -0,0 +1,4 @@
+﻿// Показать вторую цифру трехзначного числа
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine($"Вторая цифра этого числа: {(a/10)%10}");
