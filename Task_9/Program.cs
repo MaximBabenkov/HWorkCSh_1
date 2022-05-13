a67a34c82e8ea921eafebe6d83e264da65bc808d
MaximBabenkov <lostbass@mail.ru>
2022-05-13 23:14:07 +0300

    Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти третью цифру числа или сообщить, что ее нет
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()??"0");
+if (a>99)
+{
+    while (a>1000)
+    {
+        a=a/10;
+    }
+    Console.WriteLine($"Третья цифра этого числа: {a%10}");
+}
+else
+{
+    Console.WriteLine("В этом числе нет третьей цифры");
+}
